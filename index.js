a425f68df685a5e5180414312eb2ad641c0eb617
joshuablockchain <joshuapentinio16@gmail.com>
2018-10-19 16:19:42 +0800

    Initialize demo app

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+
+let dogs = [];
+
+
+app.get('/:index', (req,res) => res.status(200).json({dog:dogs[req.params.index]}));
+
+app.get('/', (req,res) => res.status(200).json({dogs: dogs}));
+app.post('/', (req,res) = >{
+
+dogs.push(req.body.dogName);
+
+
+});
+
+
+
+
+/*
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/',(req,res) => {
+   const name = req.body.name;
+   res.send(`Hello ${name[4]}!`);
+});
+
+app.get('/', (req, res) => {
+    res.getElementById("DEMO-APP").innerHTML = name;
+});
+
+
+app.listen(port, () => {
+    console.log('Running at port 3000!');
+});
+*/
