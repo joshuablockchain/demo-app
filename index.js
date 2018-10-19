968be39939fc5f356f8f4dad9a63cd6a52eb942d
joshuablockchain <44281582+joshuablockchain@users.noreply.github.com>
2018-10-19 17:45:31 +0800

    Updated index.js

--- index.js
+++ index.js
@@ -10,21 +10,23 @@ let dogs = [];
 app.get('/:index', (req,res) => res.status(200).json({dog:dogs[req.params.index]}));
 
 app.get('/', (req,res) => res.status(200).json({dogs: dogs}));
-app.post('/', (req,res) = >{
+app.post('/', (req,res) => {
 
 dogs.push(req.body.dogName);
+res.status(200).json({status: `Added ${req.body.dogName}!`});
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 });
 
 
 
 
-/*
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
+/*
 app.post('/',(req,res) => {
    const name = req.body.name;
    res.send(`Hello ${name[4]}!`);
@@ -33,9 +35,8 @@ app.post('/',(req,res) => {
 app.get('/', (req, res) => {
     res.getElementById("DEMO-APP").innerHTML = name;
 });
-
+*/
 
 app.listen(port, () => {
     console.log('Running at port 3000!');
 });
-*/
