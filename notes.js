fe50ebad5867db38c8d37413a695ab2a9c9e1fea
joshuablockchain <44281582+joshuablockchain@users.noreply.github.com>
2018-10-23 14:30:32 +0800

    storeee

--- notes.js
+++ notes.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-var notes = [];
-let idCtr = 0;
+const simpleJsonStore = require("simple-json-store");
+
+const store = new simpleJsonStore('./data.json', { notes: [] });
+
 router.get('/', (req,res) =>{
-    res.json(notes);
+    res.json(store.get('notes'));
 });
 
 router.post('/',(req,res)=>{
-    idCtr += 1;
+    const notes = store.get("notes");
+
+
     var newNote = {
-        id: idCtr,
+        id: notes.length > 0 ? (notes[notes.length - 1].id + 1) : 1,
         title: req.body.title,
         description: req.body.description
     };
+
     notes.push(newNote);
-    res.json(notes);
+    store.set("notes", notes);
 });
 
 router.get('/:id',(req,res) =>{
     const id = req.params.id;
-    let note = notes.find(function (note){
-        return note.id == id;
-    });
+    const notes = store.get("notes");
+    // const note = notes.find(note => note.id == id);
+    // let note = notes.find(function (note){
+    //     return note.id == id;
+    // });
 
 
     // for(let i = 0; i < notes.length; i++){
@@ -34,5 +41,21 @@ router.get('/:id',(req,res) =>{
 
     res.json(note);
 });
+
+router.put('/id', (req,res) =>{
+
+    const id = req.params.id;
+    const notes = store.get("notes");
+
+    let note = {};
+
+    for (let i = 0; i < notes.length; i++){
+        if(notes[i].id == id){
+            notes[i].title = req.body.title;
+            notes[i].description = req.body.description;
+        }
+    }
+});
+
 
 module.exports = router;
